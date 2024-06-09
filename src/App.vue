95f6d018c7f26118c5f3450da2e418c003f6f3af
vartikus1337 <pasha_klevtsov@vk.com>
2024-06-09 21:49:18 +0300

    Del @ts-config and Add Shop

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-// @ts-ignore
 import Header from "@/components/Header/TheHeader.vue";
 import Interesting from "./components/Interesting/TheInteresting.vue";
+import Shop from "./components/Shop/TheShop.vue";
 </script>
 
 <template>
   <Header />
   <Interesting />
+  <Shop />
 </template>
 
 <style scoped></style>
